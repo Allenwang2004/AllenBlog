9bfcc027f0f641ef010ed2d36d0f179a185e7dbf
Allenwang2004 <ziyiw1930@gmail.com>
2025-01-22 21:04:18 +0800

    $

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://nextjs-tailwind-contentlayer-blog-starter.vercel.app',
+  siteUrl: 'https://allen-blog-omega.vercel.app',
   generateRobotsTxt: true,
 };
