02c282fd74aaeb19bb4ef3124b80b2e09dc45024
Allenwang2004 <ziyiw1930@gmail.com>
2025-01-22 20:56:17 +0800

    $

--- src/configs/siteConfigs.ts
+++ src/configs/siteConfigs.ts
@@ -1,6 +1,6 @@
 const fqdn = 'https://nextjs-tailwind-contentlayer-blog-starter.vercel.app';
-const logoPath = '/logo.png';
-const bannerPath = '/og-image.png';
+const logoPath = '/logo.jpg';
+const bannerPath = '/og.jpg';
 
 export const siteConfigs = {
   title: 'Allen Blog',
@@ -11,10 +11,10 @@ export const siteConfigs = {
   credit: 'Allenwang',
   email: 'ziyiw1930@gmail.com',
   fqdn: fqdn,
-  //logoPath: logoPath,
-  //logoUrl: fqdn + logoPath,
-  //bannerPath: bannerPath,
-  //bannerUrl: fqdn + bannerPath,
+  logoPath: logoPath,
+  logoUrl: fqdn + logoPath,
+  bannerPath: bannerPath,
+  bannerUrl: fqdn + bannerPath,
   igID: '@w_yizi',
   datePublished: '2025-01-21',
 };
